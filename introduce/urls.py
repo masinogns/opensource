b91491b2c93263af1a06b69bc42dab5a4bbda768
b.kihoon <masinogns@gmail.com>
2017-06-11 17:10:51 +0900

    introduce url, view working

--- introduce/urls.py
+++ introduce/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     # url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^$', IndexView, name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^story/', StoryView.as_view(), name='story'),
+    url(r'^effect/', EffectView.as_view(), name='effect'),
+    url(r'^future/', FutureView.as_view(), name='future'),
 ]
--- introduce/views.py
+++ introduce/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'introduce/index.html'
 
-def IndexView(request):
-    return render(request, 'introduce/index.html')
+class StoryView(TemplateView):
+    template_name = 'introduce/story.html'
+
+class EffectView(TemplateView):
+    template_name = 'introduce/effect.html'
+
+class FutureView(TemplateView):
+    template_name = 'introduce/future.html'
