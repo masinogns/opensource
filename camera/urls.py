7fdf5b02c21bb8a2350961278f9717d555e43726
b.kihoon <masinogns@gmail.com>
2017-06-11 17:48:05 +0900

    camera url, view working finish

--- camera/urls.py
+++ camera/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     # url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^$', IndexView, name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^situation/', SituationView.as_view(), name='situation'),
+    url(r'^different/', DifferentView.as_view(), name='different'),
+    url(r'^prevent/', PreventView.as_view(), name='prevent'),
 ]
--- camera/views.py
+++ camera/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'camera/index.html'
 
-def IndexView(request):
-    return render(request, 'camera/index.html')
+class SituationView(TemplateView):
+    template_name = 'camera/situation.html'
+
+class DifferentView(TemplateView):
+    template_name = 'camera/different.html'
+
+class PreventView(TemplateView):
+    template_name = 'camera/prevent.html'
